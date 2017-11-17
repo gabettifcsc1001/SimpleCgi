806f38387f91f582b3cdd70ea98cea0a194d9e78
Tachibana Yuji <kanful.28@gmail.com>
2017-11-18 02:03:40 +0900

    プロジェクトフォルダの作成

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"test.h"
+
+/**
+ * [main  description]
+ * @return [description]
+ */
+int main () {
+  printf("Hello World!!\n");
+
+  TEST obj("your name");
+
+  obj.print();
+
+  return 0;
+}
